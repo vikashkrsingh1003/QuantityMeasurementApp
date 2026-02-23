e968367f6e8c10dba68d3518daba170087854187
vikash <vksingh1062003@gmail.com>
2026-02-23 14:46:15 +0530

    [vikash]: added program for UC4 extend  version

--- src/main/java/com/apps/quantitymeasurement/Length.java
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -10,7 +10,10 @@ public class Length {
     // Enum for supported length units (base unit = inches)
     public enum LengthUnit {
         FEET(12.0),
-        INCHES(1.0);
+        INCHES(1.0),
+    	YARDS(36.0),         // 1 yard = 36 inches
+        CENTIMETERS(0.393701); // 1 cm = 0.393701 inches
+
 
         private final double conversionFactorToInches;
 

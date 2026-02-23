b05a2b4316d5c9fec819443a2cf6c690f2ac94a6
vikash <vksingh1062003@gmail.com>
2026-02-23 13:29:05 +0530

    [vikash]: added program for UC3

---
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -0,0 +1,77 @@
+package com.apps.quantitymeasurement;
+
+import java.util.Objects;
+
+public class Length {
+
+    private final double value;
+    private final LengthUnit unit;
+
+    // Enum for supported length units (base unit = inches)
+    public enum LengthUnit {
+        FEET(12.0),
+        INCHES(1.0);
+
+        private final double conversionFactorToInches;
+
+        LengthUnit(double conversionFactorToInches) {
+            this.conversionFactorToInches = conversionFactorToInches;
+        }
+
+        public double toInches(double value) {
+            return value * conversionFactorToInches;
+        }
+    }
+
+    // Constructor
+    public Length(double value, LengthUnit unit) {
+
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null");
+        }
+
+        if (value < 0) {
+            throw new IllegalArgumentException("Length cannot be negative");
+        }
+
+        this.value = value;
+        this.unit = unit;
+    }
+
+    // Convert any length to base unit (inches)
+    private double toInches() {
+        return unit.toInches(value);
+    }
+
+    // Compare two Length objects
+    public boolean compare(Length that) {
+
+        if (that == null) {
+            return false;
+        }
+
+        return Double.compare(this.toInches(), that.toInches()) == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (!(o instanceof Length)) return false;
+
+        Length that = (Length) o;
+
+        return Double.compare(this.toInches(), that.toInches()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(toInches());
+    }
+
+    @Override
+    public String toString() {
+        return "Quantity(" + value + ", \"" + unit.name().toLowerCase() + "\")";
+    }
+}
